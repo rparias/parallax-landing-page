58d28c11e9e57541ffc8525aa1cee51803a230cc
Ronald Arias <ariasron@hotmail.com>
2022-07-15 14:50:42 -0500

    adds functionality to submit email form

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import "./style.css";
 
-// IMAGE SLIDER //
+// ------------------ IMAGE SLIDER ------------------  //
 const slideBtns = document.querySelectorAll("[data-slideBtn]");
 const slideContainer = document.querySelector("[data-slideContainer]");
 const slides = [...document.querySelectorAll("[data-slide]")];
@@ -57,3 +57,48 @@ const slideObserver = new IntersectionObserver(
 );
 
 slideObserver.observe(slides[slides.length - 1]);
+
+// ------------------ END IMAGE SLIDER ------------------  //
+
+// ------------------ FORM HANDLE ------------------  //
+const contactForm = document.querySelector("#contact-form");
+const contactBtn = document.querySelector("#contact-btn");
+const contactInput = document.querySelector("#email");
+
+// functions
+function postEmailToDatabase(email) {
+  console.info(`Your email is ${email}`);
+  return new Promise(resolve => setTimeout(resolve, 1000));
+}
+
+const contactBtnOptions = {
+  pending: `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="animate-spin" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="128" y1="32" x2="128" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="224" y1="128" x2="192" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="195.9" y1="195.9" x2="173.3" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="128" y1="224" x2="128" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="60.1" y1="195.9" x2="82.7" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="32" y1="128" x2="64" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line><line x1="60.1" y1="60.1" x2="82.7" y2="82.7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"></line></svg>
+    <span class="uppercase tracking-wide animate-pulse">
+      Sending...
+    </span>
+  `,
+  success: `
+    <span class="uppercase tracking-wide animate-pulse">
+      Thank you!
+    </span>
+    <span class="uppercase tracking-wide animate-pulse">
+      ✌️
+    </span>
+  `
+}
+
+async function handleFormSubmit(e) {
+  e.preventDefault();
+  addDisabledAttribute([contactForm, contactBtn]);
+  contactBtn.innerHTML = contactBtnOptions.pending;
+  const userEmail = contactInput.value;
+  contactInput.style.display = "none";
+  await postEmailToDatabase(userEmail);
+  contactBtn.innerHTML = contactBtnOptions.success;
+}
+
+// event listeners
+contactForm.addEventListener("submit", handleFormSubmit);
+
+// ------------------ END FORM HANDLE ------------------  //
